0960f09cc1011c6e1bfceff57f84095032cc47ec
Enioluwa Segun <eos24@drexel.edu>
2017-09-05 21:34:58 -0400

    GMaps API implementation
    Added the Gmaps request functionality under '/route'. It currently does not send any response.

--- server.js
+++ server.js
@@ -1,17 +1,45 @@
 var mysql = require('mysql');
-var http = require('http');
+var https = require('https');
 var express = require('express');
 var fs = require('fs');
+
 var app = express();
+
 var con = mysql.createConnection({
         host: 'localhost',
         user: 'zelon',
         database: 'hw5'
     });
 
+var gApiKey = fs.readFileSync('./gmaps_key','utf8');
+var GoogleMaps = require('@google/maps').createClient({key: gApiKey});
+	
+	
 app.use(express.static("./Home"));
+
 app.listen(8080);
+
 app.get('/', function (req, res){
         var data = fs.readFileSync('./index.html','utf8');
         res.end(data);
     });
+	
+	
+app.get('/route', function(req, res){
+	console.log("Received request for train directions.");
+	var options = {
+				host: 'maps.googleapis.com',
+				path: '/api/directions/json?' + req.query.info
+	};
+	
+	https.request(options, function(resp) {
+		var out = '';
+		resp.on('data', function(chunk){out += chunk;});
+		resp.on('end', function() {/*needs to be impelented. json parsed to string, etc*/});
+	}).end();
+		
+	
+		
+	
+	
+});
